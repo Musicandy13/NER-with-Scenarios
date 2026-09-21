ac943b7eb0a8d3b4e4dba3bf3141a10aff945243
Musicandy13 <132613852+Musicandy13@users.noreply.github.com>
2026-09-21 21:57:54 +0200

    Fine-tune Google Play badge size

--- src/App.jsx
+++ src/App.jsx
@@ -762,7 +762,7 @@ return (
               <img
                 src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
                 alt="Get it on Google Play"
-                className="h-16 w-auto"
+                className="h-[72px] w-auto"
               />
             </a>
           </div>
